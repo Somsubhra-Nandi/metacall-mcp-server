090ca4730075f41f28d683eaef4459e6ae294cea
Somsubhra-Nandi <nruma325@gmail.com>
2026-03-07 13:29:23 +0530

    await tool export

--- src/server/handlers/index.ts
+++ src/server/handlers/index.ts
@@ -14,6 +14,7 @@ import { fileListTool } from "./fileList.js";
 import { listSubscriptionsDeploysTool } from "./listSubscriptionsDeploys.js";
 import { callTool } from "./call.js";
 import { invokeTool } from "./invoke.js";
+import { awaitTool } from "./await.js";
 
 //export all tools
 export const tools = [
@@ -32,5 +33,6 @@ export const tools = [
   fileListTool,
   listSubscriptionsDeploysTool,
   callTool,
-  invokeTool
+  invokeTool,
+  awaitTool
 ];
